83d4f3f628447823c30a534d3c0636016934e950
Rafael Monteiro Arjonas <rafaelmonteiroarj@gmail.com>
2021-07-16 21:11:03 -0300

    ✨ Add apollo utils to work with SSR/SSG...

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,11 @@ import Head from 'next/head';
 import { useRouter } from 'next/router';
 import NProgress from 'nprogress';
 import { useEffect } from 'react';
+import { ApolloProvider } from '@apollo/client';
 
 import GlobalStyles from 'styles/global';
 import ResetStyle from 'styles/reset';
+import { useApollo } from 'utils/apollo';
 
 function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
@@ -30,26 +32,30 @@ function App({ Component, pageProps }: AppProps) {
     };
   }, [router]);
 
+  const client = useApollo(pageProps.initialApolloState);
+
   return (
     <>
-      <Head>
-        <title>React Avançado - Boilerplate</title>
-        <link rel="shortcut icon" href="/img/icon-512.png" />
-        <link rel="apple-touch-icon" href="/img/icon-512.png" />
-        <link rel="manifest" href="/manifest.json" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400&family=Exo+2:wght@700&family=Lato:wght@100;400&display=swap"
-          rel="stylesheet"
-        />
-        <meta
-          name="description"
-          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
-        />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
-      <ResetStyle />
-      <GlobalStyles />
-      <Component {...pageProps} />
+      <ApolloProvider client={client}>
+        <Head>
+          <title>React Avançado - Boilerplate</title>
+          <link rel="shortcut icon" href="/img/icon-512.png" />
+          <link rel="apple-touch-icon" href="/img/icon-512.png" />
+          <link rel="manifest" href="/manifest.json" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400&family=Exo+2:wght@700&family=Lato:wght@100;400&display=swap"
+            rel="stylesheet"
+          />
+          <meta
+            name="description"
+            content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+          />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+        </Head>
+        <ResetStyle />
+        <GlobalStyles />
+        <Component {...pageProps} />
+      </ApolloProvider>
     </>
   );
 }
